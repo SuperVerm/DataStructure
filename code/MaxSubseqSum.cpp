164a9575dde45b6916ee994ac713c102a718d602
SuperVerm <978692527@qq.com>
2022-10-09 16:32:28 +0800

    最大子列和

--- code/MaxSubseqSum.cpp
+++ code/MaxSubseqSum.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-//原始办法，i指向子列头，j指向子列尾，全部遍历。
-int MaxSubseqSum1(int n, int a[])
+//原始办法，i指向子列头，全部遍历。时间复杂度O(n*2)。
+int MaxSubseqSum01(int n, int a[])
 {
     int max = 0;
     for(int i = 0; i < n; i++)
@@ -21,14 +21,35 @@ int MaxSubseqSum1(int n, int a[])
     return max;
 }
 
+//最优办法，一次遍历。前者累和小于0就抛弃。时间复杂度O(n)。
+int MaxSubseqSum02(int n, int a[])
+{
+    int max = 0;
+    int temp = 0;
+    for(int i = 0; i < n; i++)
+    {
+        temp += a[i];
+        if(temp < 0)
+        {
+            temp = 0;
+        }else if(temp > max)
+        {
+            max = temp;
+        }
+    }
+    return max;
+}
+
 int main()
 {
-    int a[] = {1,2,-3,6};
+    int a[] = {5, 2, -3, 6};
     //计算数组的长度n。
     int n = sizeof(a) / sizeof(a[0]);
 
-    int result = MaxSubseqSum1(n,a);
-    cout << "最大的子列和为" << result << endl;
+    int result01 = MaxSubseqSum01(n, a);
+
+    int result02 = MaxSubseqSum02(n, a);
+    cout << "最大的子列和为" << result02 << endl;
     system("pause");
     return 0;
 }
